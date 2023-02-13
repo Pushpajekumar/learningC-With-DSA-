958f3c2bc3fd42f2dcc6a95516add6c0a9d9700b
Pushpajekumar <mk6358522@gmail.com>
2023-02-13 20:21:57 +0530

    Hello world

---
+++ Hello World/01_helloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "This is Pushpaje Kumar" ;
+    return 0;
+}
---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int amount1 = 45;
+    int amount2 = 55;
+    int sum;
+sum = amount1 + amount2;
+cout << sum;
+// cin >> amount1 >> amount2;
+// cout<<"The value of amount 1 is" << amount1 ;
+// cout<<"The value of amount 2 is" << amount2 ;
+    return 0;
+}
