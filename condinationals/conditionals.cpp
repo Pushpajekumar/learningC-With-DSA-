4e51e921bb05ee59a173fdf961651712e59a85e6
Pushpajekumar <mk6358522@gmail.com>
2023-02-14 20:41:08 +0530

    conditionals

---
+++ condinationals/conditionals.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int age = 50;
+    if( age > 50) {
+cout<< "You are younger";
+    } else if( age < 100) {
+cout<< "You are very old";
+    }
+}
